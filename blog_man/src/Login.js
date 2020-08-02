e0fb81116de10f94ec1f5957b3d8c71e82fed11e
fengjutian <2640111081@qq.com>
2020-08-02 18:08:26 +0800

    save

--- blog_man/src/Login.js
+++ blog_man/src/Login.js
@@ -1,9 +1,89 @@
 import React from 'react';
+import './Login.css';
+import { Layout } from 'antd';
+const { Header, Footer, Content, Form, Input, Button, Checkbox } = Layout;
+
+const layout = {
+    labelCol: {
+        span: 8,
+    },
+    wrapperCol: {
+        span: 16,
+    },
+};
+const tailLayout = {
+    wrapperCol: {
+        offset: 8,
+        span: 16,
+    },
+};
 class Login extends React.Component {
+    
     render() {
-        return <h1>ffhhhh</h1>
+        const onFinish = values => {
+            console.log('Success:', values);
+          };
+        
+          const onFinishFailed = errorInfo => {
+            console.log('Failed:', errorInfo);
+          };
+        
+        return (<Layout>
+                    <Header className='top'></Header>
+                    <Content className='ctx'>
+                        <div class='input-box'>
+                        <Form
+                            {...layout}
+                            name="basic"
+                            initialValues={{
+                                remember: true,
+                            }}
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                            >
+                            <Form.Item
+                                label="Username"
+                                name="username"
+                                rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your username!',
+                                },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                                ]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+
+                            <Form.Item {...tailLayout}>
+                                <Button type="primary" htmlType="submit">
+                                Submit
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                        </div>
+                    </Content>
+                    <Footer class='bottom'>Footer</Footer>
+                </Layout>);
     }
 }
+
 
 export default Login;
 
